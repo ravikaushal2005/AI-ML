9afc53323b044ef5bb7df7e24c365a0f629ef931
Ravi kaushal <ravikaushal7284@gmail.com>
2026-06-11 05:02:53 +0000

    mistake

--- python/basics/B_variable.py
+++
@@ -1,9 +0,0 @@
-#variable
-''' variables are used to store data values it is a container for storing data values. it is a name given to a memory location where data is stored. '''
-
-a=10
-print(a)
-b=3.14
-print(b)
-c="hello"
-print(c)            
